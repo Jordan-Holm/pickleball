19799fcb07b03e064760ec61ea7d0762ac63ec17
Andrew Gaertner <Andrewjgaertner01@gmail.com>
2022-10-08 14:55:11 -0600

    Added navbar added the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/shared/home/Home';
+import Nomatch from './components/shared/Nomatch';
+import Mainnavbar from './components/shared/Mainnavbar';
+import Login from './components/auth/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <>
+    <Mainnavbar />
     <Routes>
       <Route path='/' element={ <Home />} />
+      <Route path='/login' element={ <Login />} />
+      <Route path='/*' element={<Nomatch />} />
     </Routes>
   </>
 
--- client/src/components/shared/home/Home.js
+++ client/src/components/shared/home/Home.js
@@ -4,7 +4,6 @@ const Home = () => (
         <h1>Home Page</h1>
         <Tournaments />
     </>
-
 )
 
 export default Home;
