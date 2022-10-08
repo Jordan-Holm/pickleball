701d60eab668fc3495ae53cfd06d4160a8848f5d
Andrew Gaertner <Andrewjgaertner01@gmail.com>
2022-10-08 10:17:49 -0600

    finished tournament controller

---
+++ app/controllers/api/tournament_controller.rb
@@ -0,0 +1,42 @@
+class Api::TournamentController < ApplicationController
+ before_action :set_tour, only: [:show, :update, :destroy]
+  def index
+    render json: Tournament.all
+  end
+
+  def show
+    render json: @tournament
+  end
+
+  def create
+    @tournament = Tournament.new(tour_params)
+    if @tournament.save
+      render json: @tournament
+    else
+      render json: { errors: @tournament.error }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @tournament.update(tour_params)
+      render json: @tournament
+    else
+      render json: { errors: @tournament.error }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tournament.destroy
+    render json: { message: 'Tournament deleted'}
+  end
+
+
+  private
+    def tour_params
+      params.require(:tournament).permit(:tname, :division, :where, :when, :gender )
+    end
+
+    def set_tour
+      @tournament = Tournament.find(params[:id])
+    end
+  end
