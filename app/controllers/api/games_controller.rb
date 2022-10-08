c6ddc2832e8611a8019eb8ff2234f67fa3d19ed5
Christian Green <hoolio.suare@gmail.com>
2022-10-08 10:49:32 -0600

    games controller

---
+++ app/controllers/api/games_controller.rb
@@ -0,0 +1,47 @@
+class Api::GamesController < ApplicationController
+  before_action :set_parent
+  before_action :set_games, only: [:show, :update, :destroy]
+
+  def index
+    render json: @tour.games
+  end
+
+  def show
+    render json: @game
+  end
+
+  def create
+    @game = @tour.games.new(game_params)
+    if @game.save
+      render json: @game
+    else
+      render json: { errors: @game.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @game.update(game_params)
+      render json: @game
+    else
+      render json: { errors: @game.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @game.destroy
+    render json: { message: 'game deleted' }
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:placements, :time)
+    end
+
+    def set_parent
+      @tour = Tournament.find(params[:tournament_id]
+    end
+
+    def set_games
+      @game = @tour.games.find(params[:id])
+    end
+end
